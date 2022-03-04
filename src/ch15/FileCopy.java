2a54083bbe63679e7d3ccfd89baace5241534ba9
Geonyeol Park <msmn1729@gmail.com>
2022-03-04 22:56:32 +0900

    File I/O Stream

---
+++ src/ch15/FileCopy.java
@@ -0,0 +1,20 @@
+package ch15;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class FileCopy {
+    public static void main(String[] args) throws IOException {
+        FileInputStream fis = new FileInputStream(args[0]);
+        FileOutputStream fos = new FileOutputStream(args[1], true);
+
+        int data = 0;
+        while ((data = fis.read()) != -1) {
+            fos.write(data); // void write(int b)
+        }
+
+        fis.close();
+        fos.close();
+    }
+}
