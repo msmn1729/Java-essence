d13d25188f8fb68d8b2d0c4df8a1fe2027d6d5e1
Geonyeol Park <msmn1729@gmail.com>
2022-03-10 23:04:35 +0900

    DataOutputStream

---
+++ src/ch15/DataOutputStreamEx1.java
@@ -0,0 +1,21 @@
+package ch15;
+
+import java.io.DataOutputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class DataOutputStreamEx1 {
+    public static void main(String[] args) throws IOException {
+        FileOutputStream fos = null;
+        DataOutputStream dos = null;
+
+        fos = new FileOutputStream("sample.dat");
+        dos = new DataOutputStream(fos);
+        dos.writeInt(10);
+        dos.writeFloat(20.0f);
+        dos.writeBoolean(true);
+
+        dos.close();
+    }
+}
