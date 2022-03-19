e0e19d477f846f4927c8af74e0608cda2eb7303d
Geonyeol Park <msmn1729@gmail.com>
2022-03-20 02:19:50 +0900

    직렬화(Serialization) : 객체를 데이터 스트림으로 만든다.

---
+++ src/ch15/SerialEx1.java
@@ -0,0 +1,28 @@
+package ch15;
+
+import java.io.*;
+import java.util.ArrayList;
+
+public class SerialEx1 {
+    public static void main(String[] args) throws IOException {
+        String fileName = "UserInfo.ser";
+        FileOutputStream fos = new FileOutputStream(fileName);
+        BufferedOutputStream bos = new BufferedOutputStream(fos, 8192);
+
+        ObjectOutputStream out = new ObjectOutputStream(bos);
+
+        UserInfo u1 = new UserInfo("JavaMan", "1234", 30);
+        UserInfo u2 = new UserInfo("JavaWoman", "4321", 26);
+
+        ArrayList<UserInfo> list = new ArrayList<>();
+        list.add(u1);
+        list.add(u2);
+
+        // 객체를 직렬화한다.
+        out.writeObject(u1);
+        out.writeObject(u2);
+        out.writeObject(list);
+        out.close();
+        System.out.println("직렬화가 잘 끝났습니다.");
+    }
+}
---
+++ src/ch15/SerialEx2.java
@@ -0,0 +1,25 @@
+package ch15;
+
+import java.io.*;
+import java.util.ArrayList;
+
+public class SerialEx2 {
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        String fileName = "UserInfo.ser";
+        FileInputStream fis = new FileInputStream(fileName);
+        BufferedInputStream bis = new BufferedInputStream(fis, 8192);
+
+        // 역직렬화
+        ObjectInputStream in = new ObjectInputStream(bis);
+
+        // 객체를 읽을 때는 출력한 순서와 일치해야한다.
+        UserInfo u1 = (UserInfo) in.readObject();
+        UserInfo u2 = (UserInfo) in.readObject();
+        ArrayList list = (ArrayList) in.readObject();
+
+        System.out.println(u1);
+        System.out.println(u2);
+        System.out.println(list);
+        in.close();
+    }
+}
