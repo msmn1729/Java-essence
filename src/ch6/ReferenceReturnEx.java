f4004052d301a9f1c0994762fde8b3ef261398b1
Geonyeol Park <msmn1729@gmail.com>
2022-05-25 23:49:23 +0900

    참조변수 타입 리턴

--- src/ch6/ReferenceReturnEx.java
+++ src/ch6/ReferenceReturnEx.java
@@ -10,24 +10,26 @@ public class ReferenceReturnEx {
         data1.value = 7;
 
         Data data2 = copy1(data1);
-        System.out.println(data1 + " " + data1.value);
-        System.out.println(data2 + " " + data2.value);
+        System.out.println(data1 + " " + data1.value); // data1의 주솟값 7
+        System.out.println(data2 + " " + data2.value); // 9
         System.out.println("\n");
 
         Data data3 = copy2(data1);
-        System.out.println(data1 + " " + data1.value);
-        System.out.println(data3 + " " + data3.value);
+        System.out.println(data1 + " " + data1.value); // 9
+        System.out.println(data3 + " " + data3.value); // 0
     }
 
     static Data copy1(Data data) {
         Data tmp = new Data();
         tmp.value = data.value; // 이 경우에는 data1, data2 참조변수의 주솟값이 다르다
+        tmp.value = 9;
         return tmp;
     }
 
     static Data copy2(Data data) {
-        Data tmp = new Data();
-        tmp = data; // 이 경우에는 data1, data3 참조변수의 주솟값이 같다
+        Data tmp = data;
+        tmp = new Data(); // 이 경우에는 data1, data3 참조변수의 주솟값이 같다
+        data.value = 9;
         return tmp;
     }
 }
