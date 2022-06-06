54eb66da18f15f2e3e0cf2dbbd837cf1af09ffef
Geonyeol Park <msmn1729@gmail.com>
2022-06-06 23:01:01 +0900

    ArrayList의 생성자와 메서드

---
+++ src/ch11/ArrayListEx2.java
@@ -0,0 +1,23 @@
+package ch11;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListEx2 {
+    public static void main(String[] args) {
+        final int LIMIT = 10;
+        String source = "124214kn21klcn2k1ln421knwdacksljajkl";
+        int length = source.length();
+
+        List list = new ArrayList(length / LIMIT + 10);
+
+        for (int i = 0; i < length; i += LIMIT) {
+            if (i + LIMIT < length) {
+                list.add(source.substring(i, i + LIMIT));
+            } else {
+                list.add(source.substring(i));
+            }
+        }
+        System.out.println(list);
+    }
+}
